e9e54f979f1882f27009fe322b98923016daa98c
Jose Neto <joseneto200462@gmail.com>
2025-09-15 10:25:10 -0300

    Exercício 1: Declare duas variáveis chamadas idade e peso, atribua valores a elas e imprima os valores na console.

---
+++ TodasAtividades/Atividade01.js
@@ -0,0 +1,16 @@
+//Exercício 1: Declare duas variáveis chamadas "idade" e "peso", atribua valores a elas e imprima os valores na console.
+/*let idade = 21;
+let peso = 81;
+
+console.log(idade);
+console.log(peso);
+*/
+    const pessoa = {
+        peso : 81,
+        idade : 21
+    };
+
+    console.log(pessoa.peso);
+    console.log(pessoa.idade);
+
+    console.log(pessoa)
