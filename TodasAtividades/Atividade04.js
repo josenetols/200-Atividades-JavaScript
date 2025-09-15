b3388316e1c7661f9017d3b67ef64c1f71ab497a
Jose Neto <joseneto200462@gmail.com>
2025-09-15 10:56:00 -0300

    Declare duas variáveis chamadas nome e sobrenome, atribua o seu nome e sobrenome a elas. Concatene-as em uma terceira variável chamada nomeCompleto e imprima.

---
+++ TodasAtividades/Atividade04.js
@@ -0,0 +1,9 @@
+//Exercício 4: Declare duas variáveis chamadas "nome" e "sobrenome", atribua o seu nome e sobrenome a elas. 
+// Concatene-as em uma terceira variável chamada "nomeCompleto" e imprima.
+
+let nome = "José Neto";
+let sobrenome = "De Oliveira Silva";
+
+
+
+console.log(`${nome} ${sobrenome}`);
