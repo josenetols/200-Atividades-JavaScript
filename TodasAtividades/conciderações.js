d7c75bc1ad1fdbe54fc08c318a48868e11f0c0b5
José Neto de Oliveira Silva <joseneto200462@gmail.com>
2025-11-17 12:44:42 -0300

    conciderações.js

---
+++ TodasAtividades/conciderações.js
@@ -0,0 +1,20 @@
+
+📌 2. Os exercícios são longos e variados
+
+Os 100 exercícios englobam:
+
+Variáveis e operadores
+
+Condicionais
+
+Loops (for, while, do-while)
+
+Funções simples
+
+Funções de alta ordem
+
+Objetos
+
+Arrays e manipulação avançada
+
+Lógica mais complexa (mediana, subarray, combinações, filtragens, etc.)
